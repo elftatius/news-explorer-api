e50982c5dbbb167d83f69ce0b0788be8da09e9c4
Tatyana Tkachenko <elf.tatius@yandex.ru>
2020-11-03 19:50:52 +0600

    create user model

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const strValidator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        return strValidator.isEmail(v);
+      },
+      message: 'Это не email',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
